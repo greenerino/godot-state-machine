7f9dde5a14c811bb3762d993feeee4521ee8eee2
greenerino <greenejustint@gmail.com>
2024-05-18 21:10:35 -0700

    Hook up death animation

--- example/skeleton_warrior.gd
+++ example/skeleton_warrior.gd
@@ -4,14 +4,15 @@ extends CharacterBody3D
 @onready var sm: StateMachine = %StateMachine
 @onready var hit_state: HitState = %StateMachine/HitState
 
-const RUN_SPEED = 5.0
+const RUN_SPEED := 5.0
+const BLEND_POSITION_PATH := "parameters/IdleRun/blend_position"
 
 func _physics_process(_delta: float) -> void:
 	if sm.curr_state is KnockbackState or velocity.length() == 0:
-		anim_tree["parameters/blend_position"] = 0
+		anim_tree[BLEND_POSITION_PATH] = 0
 	elif velocity.length() > 0:
 		rotation.y = atan2(velocity.x, velocity.z)
-		anim_tree["parameters/blend_position"] = velocity.length() / RUN_SPEED
+		anim_tree[BLEND_POSITION_PATH] = velocity.length() / RUN_SPEED
 
 func _on_hurt_box_area_entered(area: Area3D) -> void:
 	sm.change_state_with_data(
